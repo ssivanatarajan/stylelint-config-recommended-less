6bcd404e5a3b7abbe551ec546a0b098ea717e0ee
sivanatarajan.s <sivanatarajan.s@zohocorp.com>
2021-06-28 21:37:49 +0530

    added at-rule-no-unknown

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     rules: {
         "declaration-colon-space-after": "always",
         "declaration-colon-space-before": "never",
+        "at-rule-no-unknown": null,
         "less/color-no-invalid-hex": true,
         "less/no-duplicate-variables": true
     }
