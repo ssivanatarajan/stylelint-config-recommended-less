bafd17b088c3b30e5d87f1fab7f2c29966f6b0f0
sivanatarajan.s <sivanatarajan.s@zohocorp.com>
2021-07-02 22:02:27 +0530

    removed unwanted rules

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ module.exports = {
     extends: ["stylelint-config-recommended"],
     plugins: ["stylelint-less"],
     rules: {
-        "declaration-colon-space-after": "always",
-        "declaration-colon-space-before": "never",
         "at-rule-no-unknown": null,
         "less/color-no-invalid-hex": true,
         "less/no-duplicate-variables": true
