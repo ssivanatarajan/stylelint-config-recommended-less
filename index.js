4b4cd228e6b8226f5c3d4f834377c0c93ebf0517
ubermanu <manu@paca.happy-dev.fr>
2023-07-22 11:42:08 +0200

    fix peer deps, add prettier

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-    extends: ["stylelint-config-recommended"],
-    plugins: ["stylelint-less"],
-    rules: {
-        "at-rule-no-unknown": null,
-        "less/color-no-invalid-hex": true,
-        "less/no-duplicate-variables": true
-    }
+	extends: ['stylelint-config-recommended'],
+	plugins: ['stylelint-less'],
+	rules: {
+		'at-rule-no-unknown': null,
+		'less/color-no-invalid-hex': true,
+		'less/no-duplicate-variables': true,
+	},
 };
